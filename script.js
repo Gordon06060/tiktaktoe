7f5d96bb8afa732297e9cbbb240ca3ed5a674556
Gordon06060 <gordonz0913@gmail.com>
2024-07-21 20:17:22 -0700

    Implemented working tik tak toe board.
    It works in the console, and will tell you who wins.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Hello World');
+
+function displayBoard(){
+        const board = [
+            ['O', '', ''],
+            ['O', '', ''],
+            ['O', '', ''],
+        ]
+        for (const row of board){
+            return {board,row};
+        }
+    }
+
+const game = displayBoard();
+function checkWin(player){
+    for (const row of game.board){
+        if (row[0]==player && row[1]== player && row[2]== player){
+            console.log(`Player ${player} Wins`);
+        }
+    }
+
+    for (let i=0; i<3; i++){
+        if (game.board[0][i]==player && game.board[1][i]==player && game.board[2][i]==player){
+            console.log(`Player ${player} Wins`);
+        }
+    }
+    return {player};
+}
+const player1 = checkWin('X');
+const player2 = checkWin('O');
+
+
